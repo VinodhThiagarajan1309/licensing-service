a7ca253c37e2f79d47cf26ee82af59321843d62b
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-10-13 15:43:11 -0500

    Added the Different Client

--- src/main/java/com/vinapex/licensingservice/services/LicenseService.java
+++ src/main/java/com/vinapex/licensingservice/services/LicenseService.java
@@ -1,7 +1,11 @@
 package com.vinapex.licensingservice.services;
 
+import com.vinapex.licensingservice.clients.OrganizationDiscoveryClient;
+import com.vinapex.licensingservice.clients.OrganizationFeignClient;
+import com.vinapex.licensingservice.clients.OrganizationRestTemplateClient;
 import com.vinapex.licensingservice.config.ServiceConfig;
 import com.vinapex.licensingservice.model.License;
+import com.vinapex.licensingservice.model.Organization;
 import com.vinapex.licensingservice.repository.LicenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +18,15 @@ public class LicenseService {
 
     @Autowired
     LicenseRepository licenseRepository;
+
+    @Autowired
+    OrganizationDiscoveryClient organizationDiscoveryClient;
+
+    @Autowired
+    OrganizationFeignClient organizationFeignClient;
+
+    @Autowired
+    OrganizationRestTemplateClient organizationRestClient;
 
     @Autowired
     ServiceConfig config;
@@ -39,5 +52,41 @@ public class LicenseService {
 
     public void deleteLicense(License license){
         licenseRepository.delete(license);
+    }
+
+    public License getLicense(String organizationId,String licenseId, String clientType) {
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+
+        Organization org = retrieveOrgInfo(organizationId, clientType);
+
+        return license
+                .withOrganizationName( org.getName())
+                .withContactName( org.getContactName())
+                .withContactEmail( org.getContactEmail() )
+                .withContactPhone( org.getContactPhone() )
+                .withComment(config.getExampleProperty());
+    }
+
+    private Organization retrieveOrgInfo(String organizationId, String clientType){
+        Organization organization = null;
+
+        switch (clientType) {
+            case "feign":
+                System.out.println("I am using the feign client");
+                organization = organizationFeignClient.getOrganization(organizationId);
+                break;
+            case "rest":
+                System.out.println("I am using the rest client");
+                organization = organizationRestClient.getOrganization(organizationId);
+                break;
+            case "discovery":
+                System.out.println("I am using the discovery client");
+                organization = organizationDiscoveryClient.getOrganization(organizationId);
+                break;
+            default:
+                organization = organizationRestClient.getOrganization(organizationId);
+        }
+
+        return organization;
     }
 }
