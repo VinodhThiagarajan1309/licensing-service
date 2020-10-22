5d0424d586495b9b83e9707f23000f94413f0cf9
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-10-22 16:13:23 -0500

    Added Util classes

--- src/main/java/com/vinapex/licensingservice/ApexLicensingServiceApplication.java
+++ src/main/java/com/vinapex/licensingservice/ApexLicensingServiceApplication.java
@@ -1,5 +1,6 @@
 package com.vinapex.licensingservice;
 
+import com.vinapex.licensingservice.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -10,6 +11,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @RefreshScope
@@ -22,7 +26,15 @@ public class ApexLicensingServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		RestTemplate template = new RestTemplate();
+		List interceptors = template.getInterceptors();
+		if(interceptors == null) {
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+		return template;
 	}
 
 	public static void main(String[] args) {
